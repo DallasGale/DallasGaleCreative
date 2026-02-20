0e25f9f9763d3fd6c155b05247acd7a42f24cf7c
Dallas Gale <dallas.gale@dijgtal.com>
2026-02-20 12:32:26 +1100

    updated cv

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,3 @@
-import "./customElements/nav/index.js";
-import "./customElements/header/index.js";
-import "./customElements/footer/index.js";
-
 import Header from "./header/index.js";
 import ScrollEvent from "./scroll-event/index.js";
 import RecentProjects from "./recentProjects/index.js";
--- src/js/customElements/header/helpers/meridiemIndicator.mjs
+++
@@ -1,6 +0,0 @@
-function meridiemIndicator(hours) {
-  if (hours >= 0 && hours < 12) return "am";
-  else if (hours >= 12 && hours <= 23) return "pm";
-}
-
-export default meridiemIndicator;
